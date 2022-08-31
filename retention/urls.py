671159d5fde1b16379914153d5c8b68ee0c03cff
enockabere <maebaenock95@gmail.com>
2022-08-31 15:22:58 +0300

    deploy

--- retention/urls.py
+++ retention/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("retention", views.registrationRetention, name='retention'),
-    path("ApplyRetention",views.ApplyRetention,name='ApplyRetention'),
-    path("retentionDetails/<str:pk>",views.retentionDetails,name='retentionDetails'),
-    path("retentionGateway/<str:pk>",views.retentionGateway,name='retentionGateway'),
+    path("retention", views.registrationRetention.as_view(), name='retention'),
+    path("retentionDetails/<str:pk>",views.retentionDetails.as_view(),name='retentionDetails'),
+    path("retentionGateway/<str:pk>",views.retentionGateway.as_view(),name='retentionGateway'),
     path("SubmitRetention/<str:pk>",views.SubmitRetention,name='SubmitRetention'),
 ]
--- retention/views.py
+++ retention/views.py
@@ -4,157 +4,156 @@ from django.conf import settings as config
 import json
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.views import View
 
 # Create your views here.
-def registrationRetention(request):
+class UserObjectMixin(object):
+    model =None
     session = requests.Session()
     session.auth = config.AUTHS
-    userId=request.session['UserID']
-    Retention= config.O_DATA.format("/QYRetension")
-    Access_Point= config.O_DATA.format(f"/QYRegistration?$filter=User_code%20eq%20%27{userId}%27")
-    OpenProducts = []
-    Pending = []
-    Approved = []
-    Rejected =[]
-    ApprovedProd = []
-    try:
-        response = session.get(Retention, timeout=10).json()
-        for res in response['value']:
-            if res['User_code'] == request.session['UserID'] and res['Status'] == 'Open':
-                output_json = json.dumps(res)
-                OpenProducts.append(json.loads(output_json))
-            if res['User_code'] == request.session['UserID'] and res['Status'] == 'Processing':
-                output_json = json.dumps(res)
-                Pending.append(json.loads(output_json))
-            if res['User_code'] == request.session['UserID'] and res['Status'] == 'Approved':
-                output_json = json.dumps(res)
-                Approved.append(json.loads(output_json))
-            if res['User_code'] == request.session['UserID'] and res['Status'] == 'Rejected':
-                output_json = json.dumps(res)
-                Rejected.append(json.loads(output_json))
-        RetResponse = session.get(Access_Point, timeout=10).json()
-        for res in RetResponse['value']:
-            if res['User_code'] == request.session['UserID'] and res['Status'] == 'Approved':
-                output_json = json.dumps(res)
-                ApprovedProd.append(json.loads(output_json))
-    except requests.exceptions.RequestException as e:
-        messages.error(request,e)
-        print(e)
-        return redirect('Registration')
-    except KeyError as e:
-        messages.info(request,"Session Expired, Login Again")
-        print(e)
-        return redirect('login')
-    openCount = len(OpenProducts)
-    pendCount = len(Pending)
-    appCount = len(Approved)
-    rejectedCount = len(Rejected)
-    print(pendCount)
-    ctx = {"openCount":openCount,"open":OpenProducts,
-    "pendCount":pendCount,"pending":Pending,"appCount":appCount,"approved":Approved,
-    "rejectedCount":rejectedCount,"rejected":Rejected,"product":ApprovedProd}
-    return render(request,"retention.html",ctx)
+    def get_object(self,endpoint):
+        response = self.session.get(endpoint, timeout=10).json()
+        return response
 
-def ApplyRetention(request):
-    if request.method == 'POST':
+class registrationRetention(UserObjectMixin,View):
+    def get(self,request):
         try:
-            retNo = ''
-            myAction = 'insert'
-            prodNo = request.POST.get('prodNo')
-            changesToTheProduct = request.POST.get('changesToTheProduct')
-            variation = request.POST.get('variation')
-            iAgree = eval(request.POST.get('iAgree'))
-            VariationNumber = request.POST.get('VariationNumber')
+            userId=request.session['UserID']
+            Retention= config.O_DATA.format(f"/QYRetension?$filter=User_code%20eq%20%27{userId}%27")
+            response = self.get_object(Retention)
+            OpenProducts = [x for x in response['value'] if x['Status'] == 'Open']
+            Pending = [x for x in response['value'] if x['Status'] == 'Processing']
+            Approved = [x for x in response['value'] if x['Status'] == 'Approved']
+            Rejected = [x for x in response['value'] if x['Status'] == 'Rejected']
 
-            if not iAgree:
-                iAgree = False
-            if not variation:
-                variation = False
-            if not VariationNumber:
-                VariationNumber = ''
+            Access_Point= config.O_DATA.format(f"/QYRegistration?$filter=User_code%20eq%20%27{userId}%27")
+            RetResponse = self.get_object(Access_Point)
+            ApprovedProd = [x for x in RetResponse['value'] if x['Status'] == 'Approved']
 
-            response = config.CLIENT.service.Retension(retNo,myAction,request.session['UserID'],prodNo,
-            VariationNumber,changesToTheProduct,variation,iAgree)
-            print(response)
-            if response == True:
-                messages.success(request,"Saved Successfully")
-                return redirect('retention')
         except requests.exceptions.RequestException as e:
+            messages.error(request,e)
             print(e)
             return redirect('retention')
         except KeyError as e:
             messages.info(request,"Session Expired, Login Again")
             print(e)
-            return redirect('login') 
-    return redirect('retention')
-
-def retentionDetails(request,pk):
-    session = requests.Session()
-    session.auth = config.AUTHS
-    Access_Point = config.O_DATA.format("/QYRetension")
-    Retension = []
-    responses =''
-    Status=''
-
-    try:
-        response = session.get(Access_Point, timeout=10).json()
-        for res in response['value']:
-            if res['User_code'] == request.session['UserID']:
-                output_json = json.dumps(res)
-                Retension.append(json.loads(output_json))
-                for product in Retension:
-                    if product['Retension_No_'] == pk:
-                        responses = product
-                        Status = product['Status']
-    except requests.exceptions.RequestException as e:
-        messages.error(request,e)
-        print(e)
-        return redirect('Registration')
-    except KeyError as e:
-        messages.info(request,"Session Expired, Login Again")
-        print(e)
-        return redirect('login')
-    
-    ctx = {"res":responses,"status":Status}
-    return render(request,'RetentionDetails.html',ctx)
-
-def retentionGateway(request,pk):
-    session = requests.Session()
-    session.auth = config.AUTHS
-    Access_Point = config.O_DATA.format("/QYRetension")
-    Products = []
-    responses = ''
-    Status = ''
-    paid = ''
-    try:
-        response = session.get(Access_Point, timeout=10).json()
-        for res in response['value']:
-            if res['User_code'] == request.session['UserID']:
-                output_json = json.dumps(res)
-                Products.append(json.loads(output_json))
-                for product in Products:
-                    if product['Retension_No_'] == pk:
-                        responses = product
-                        Status = product['Status']
-                        paid = product['Paid']
+            return redirect('login')
+        openCount = len(OpenProducts)
+        pendCount = len(Pending)
+        appCount = len(Approved)
+        rejectedCount = len(Rejected)
+        print(pendCount)
+        ctx = {"openCount":openCount,"open":OpenProducts,
+        "pendCount":pendCount,"pending":Pending,"appCount":appCount,"approved":Approved,
+        "rejectedCount":rejectedCount,"rejected":Rejected,"product":ApprovedProd}
+        return render(request,"retention.html",ctx)
+    def post(self,request):
         if request.method == 'POST':
-            if paid == True:
-                messages.success(request,"Payment Received successfully")
-                return redirect('retentionDetails', pk=pk)
-            if paid == False:
-                messages.info(request,"Payment not received, Try again.")
+            try:
+                retNo = request.POST.get('retNo')
+                myAction = request.POST.get('myAction')
+                prodNo = request.POST.get('prodNo')
+                changesToTheProduct = request.POST.get('changesToTheProduct')
+                variation = request.POST.get('variation')
+                iAgree = eval(request.POST.get('iAgree'))
+                VariationNumber = request.POST.get('VariationNumber')
+
+                if not iAgree:
+                    iAgree = False
+                if not variation:
+                    variation = False
+                if not VariationNumber:
+                    VariationNumber = ''
+
+                response = config.CLIENT.service.Retension(retNo,myAction,request.session['UserID'],prodNo,
+                VariationNumber,changesToTheProduct,variation,iAgree)
+                print(response)
+                if response == True:
+                    messages.success(request,"Saved Successfully")
+                    return redirect('retention')
+            except requests.exceptions.RequestException as e:
+                print(e)
+                return redirect('retention')
+            except KeyError as e:
+                messages.info(request,"Session Expired, Login Again")
+                print(e)
+                return redirect('login') 
+        return redirect('retention')
+    
+
+class retentionDetails(UserObjectMixin,View):
+    def get(self,request,pk):
+        try:
+            userID =request.session['UserID']
+            Access_Point = config.O_DATA.format(f"/QYRetension?$filter=User_code%20eq%20%27{userID}%27%20and%20Retension_No_%20eq%20%27{pk}%27")
+            response = self.get_object(Access_Point)
+
+            for res in response['value']:
+                responses = res
+                Status = res['Status']
+        except requests.exceptions.RequestException as e:
+            messages.error(request,e)
+            print(e)
+            return redirect('retention')
+        except KeyError as e:
+            messages.info(request,"Session Expired, Login Again")
+            print(e)
+            return redirect('login')
+        
+        ctx = {"res":responses,"status":Status}
+        return render(request,'RetentionDetails.html',ctx)
+
+class retentionGateway(UserObjectMixin,View):
+    def get(self,request,pk):
+        try:
+            userID = request.session['UserID']
+            Access_Point = config.O_DATA.format(f"/QYRetension?$filter=User_code%20eq%20%27{userID}%27%20and%20Retension_No_%20eq%20%27{pk}%27")
+            response = self.get_object(Access_Point)
+            for res in response['value']:
+                responses = res
+                Status = res['Status']                
+        except requests.exceptions.RequestException as e:
+            messages.error(request,e)
+            print(e)
+            return redirect('retention')
+        except KeyError as e:
+            messages.info(request,"Session Expired, Login Again")
+            print(e)
+            return redirect('login')
+        ctx = {"res":responses,"status":Status}
+        return render(request,'retentionGateway.html',ctx)
+    def post(self,request,pk):
+        if request.method == 'POST':
+            try:
+                transactionCode = request.POST.get('transactionCode')
+                currency = request.POST.get('currency')
+
+                if not transactionCode:
+                    messages.error(request, "Transaction Code can't be empty.")
+                    return redirect('retentionGateway', pk=pk)
+                if not currency:
+                    messages.error(request, "Currency code missing please contact the system admin")
+                    return redirect('retentionGateway', pk=pk)
+                response = config.CLIENT.service.FnConfirmPayment(transactionCode,currency,pk,request.session['UserID'])
+                print(response)
+                if response == True:
+                    messages.success(request,"Payment was successful. You can now submit your application.")
+                    return redirect('retentionDetails', pk=pk)
+                else:
+                    messages.error("Payment Not sent. Try Again.")
+                    return redirect('retentionGateway', pk=pk)
+            except requests.exceptions.RequestException as e:
+                messages.error(request,e)
+                print(e)
                 return redirect('retentionGateway', pk=pk)
-            
-    except requests.exceptions.RequestException as e:
-        messages.error(request,e)
-        print(e)
-        return redirect('Registration')
-    except KeyError as e:
-        messages.info(request,"Session Expired, Login Again")
-        print(e)
-        return redirect('login')
-    ctx = {"res":responses,"status":Status}
-    return render(request,'retentionGateway.html',ctx)
+            except KeyError as e:
+                messages.info(request,"Session Expired, Login Again")
+                print(e)
+                return redirect('login')
+            except Exception as e:
+                messages.error(request,e)
+                return redirect('retentionGateway', pk=pk)
+        return redirect('retentionGateway', pk=pk)
 
 def SubmitRetention(request,pk):
     if request.method == 'POST':
