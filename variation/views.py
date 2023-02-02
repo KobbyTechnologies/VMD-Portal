712300580c9576dbfb2c007788956f9ca313158e
motanyaFrancis <francismotanya@outlook.com>
2023-02-02 16:37:15 +0300

    feed additives update 0202/2023

--- variation/views.py
+++ variation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 import requests
 import base64
 import json
@@ -7,49 +7,61 @@ from django.contrib import messages
 from django.views import View
 
 # Create your views here.
+
+
 class UserObjectMixin(object):
-    model =None
+    model = None
     session = requests.Session()
     session.auth = config.AUTHS
-    def get_object(self,endpoint):
+
+    def get_object(self, endpoint):
         response = self.session.get(endpoint, timeout=10).json()
         return response
 
-class variation(UserObjectMixin,View):
-    def get(self,request):
+
+class variation(UserObjectMixin, View):
+    def get(self, request):
         try:
             LTR_Name = request.session['LTR_Name']
             LTR_Email = request.session['LTR_Email']
             session = requests.Session()
             session.auth = config.AUTHS
-            userId =request.session['UserID']
-            variation= config.O_DATA.format(f"/QYVariation?$filter=User_code%20eq%20%27{userId}%27")
+            userId = request.session['UserID']
+            variation = config.O_DATA.format(
+                f"/QYVariation?$filter=User_code%20eq%20%27{userId}%27")
             response = self.get_object(variation)
-            OpenProducts = [x for x in response['value'] if x['Status'] == 'Open']
-            Pending = [x for x in response['value'] if x['Status'] == 'Processing']
-            Approved = [x for x in response['value'] if x['Status'] == 'Approved']
-            Rejected = [x for x in response['value'] if x['Status'] == 'Rejected']
+            OpenProducts = [x for x in response['value']
+                if x['Status'] == 'Open']
+            Pending = [x for x in response['value']
+                if x['Status'] == 'Processing']
+            Approved = [x for x in response['value']
+                if x['Status'] == 'Approved']
+            Rejected = [x for x in response['value']
+                if x['Status'] == 'Rejected']
 
-            Access_Point= config.O_DATA.format(f"/QYRegistration?$filter=User_code%20eq%20%27{userId}%27")
+            Access_Point = config.O_DATA.format(
+                f"/QYRegistration?$filter=User_code%20eq%20%27{userId}%27")
             VarResponse = self.get_object(Access_Point)
-            ApprovedVariation = [x for x in VarResponse['value'] if x['Status'] == 'Approved']
-            
+            ApprovedVariation = [
+                x for x in VarResponse['value'] if x['Status'] == 'Approved']
+
         except requests.exceptions.RequestException as e:
-            messages.error(request,e)
+            messages.error(request, e)
             print(e)
             return redirect('Registration')
         except KeyError as e:
-            messages.info(request,"Session Expired, Login Again")
+            messages.info(request, "Session Expired, Login Again")
             print(e)
             return redirect('login')
         openCount = len(OpenProducts)
         pendCount = len(Pending)
         appCount = len(Approved)
         rejectedCount = len(Rejected)
-        ctx = {"openCount":openCount,"open":OpenProducts,
-        "pendCount":pendCount,"pending":Pending,"appCount":appCount,"approved":Approved,
-        "rejectedCount":rejectedCount,"rejected":Rejected,"product":ApprovedVariation,"LTR_Name":LTR_Name,"LTR_Email":LTR_Email}
-        return render(request,"variation.html",ctx)
+        ctx = {"openCount": openCount, "open": OpenProducts,
+        "pendCount": pendCount, "pending": Pending, "appCount": appCount, "approved": Approved,
+        "rejectedCount": rejectedCount, "rejected": Rejected, "product": ApprovedVariation, "LTR_Name": LTR_Name, "LTR_Email": LTR_Email}
+        return render(request, "variation.html", ctx)
+
     def post(self, request):
         if request.method == 'POST':
             try:
@@ -65,40 +77,44 @@ class variation(UserObjectMixin,View):
                 proposed = request.POST.get('proposed')
                 signatoryPosition = request.POST.get('signatoryPosition')
                 signatoryName = request.POST.get('signatoryName')
-                iAgree = eval(request.POST.get('iAgree'))             
-                
+                iAgree = eval(request.POST.get('iAgree'))
 
-
-                response = config.CLIENT.service.FnVariation(varNo,myAction,request.session['UserID'],
-                        dosageForms,prodNo,typeofChange,otherApplications,scope,background,present,
-                        proposed,signatoryPosition,signatoryName,iAgree)
+                response = config.CLIENT.service.FnVariation(varNo, myAction, request.session['UserID'],
+                        dosageForms, prodNo, typeofChange, otherApplications, scope, background, present,
+                        proposed, signatoryPosition, signatoryName, iAgree)
                 print(response)
                 if response == True:
-                    messages.success(request,"Request Successful")
+                    messages.success(request, "Request Successful")
                     return redirect('variation')
             except requests.exceptions.RequestException as e:
                 print(e)
                 return redirect('variation')
             except KeyError as e:
-                messages.info(request,"Session Expired, Login Again")
+                messages.info(request, "Session Expired, Login Again")
                 print(e)
-                return redirect('login') 
+                return redirect('login')
         return redirect('variation')
 
 
-    
-class  variationDetails(UserObjectMixin,View):
-    def get(self, request,pk):
+class variationDetails(UserObjectMixin, View):
+    def get(self, request, pk):
         try:
             userID = request.session['UserID']
             LTR_Name = request.session['LTR_Name']
             LTR_Email = request.session['LTR_Email']
-            Access_Point = config.O_DATA.format(f"/QYVariation?$filter=User_code%20eq%20%27{userID}%27%20and%20Variation_No_%20eq%20%27{pk}%27")
+            Access_Point = config.O_DATA.format(
+                f"/QYVariation?$filter=User_code%20eq%20%27{userID}%27%20and%20Variation_No_%20eq%20%27{pk}%27")
             response = self.get_object(Access_Point)
-            
+
             for res in response['value']:
                 responses = res
                 Status = res['Status']
+
+            AllAttachments = config.O_DATA.format(
+                f"/QYDocumentAttachments?$filter=No_%20eq%20%27{pk}%27%20and%20Table_ID%20eq%2050004")
+            AllAttachResponse = self.get_object(AllAttachments)
+            Files = [x for x in AllAttachResponse['value']]
+
         except requests.exceptions.RequestException as e:
             messages.error(request,e)
             print(e)
@@ -108,7 +124,9 @@ class  variationDetails(UserObjectMixin,View):
             print(e)
             return redirect('login')
         
-        ctx = {"res":responses,"status":Status,"LTR_Name":LTR_Name,"LTR_Email":LTR_Email}
+        ctx = {"res":responses,"status":Status,"LTR_Name":LTR_Name,"LTR_Email":LTR_Email,
+            "files":Files,
+        }
         return render(request,'variationDetails.html',ctx)
 
 class variationGateway(UserObjectMixin,View):
@@ -190,30 +208,29 @@ def SubmitVariation(request,pk):
     return redirect('variationDetails', pk=pk)
 
 
-def FnRetentionAttachement(request, pk):
-    response = ''
+def FnVariationAttachement(request, pk):
     if request.method == "POST":
         try:
             attach = request.FILES.get('attachment')
-            fileName = request.FILES['attachment'].name
+            filename = request.FILES['attachment'].name
             name = request.POST.get('name')
             tableID = 1173
             attachment = base64.b64encode(attach.read())
 
-        
             try:
-                response = config.CLIENT.service.FnRetentionAttachement(
-                    pk, fileName,name, attachment, tableID,
-                    )
-            except Exception as e:
-                messages.error(request, e)        
-                print(e)        
+                response = config.CLIENT.service.FnVariationAttachement(
+                    pk, filename, attachment, tableID, name)
+                print(response)
                 if response == True:
-                    messages.success(request, "File1 Upload Successful")
+                    messages.success(request, "Upload Successful")
                     return redirect('variationDetails', pk=pk)
                 else:
                     messages.error(request, "Failed, Try Again")
                     return redirect('variationDetails', pk=pk)
+            except Exception as e:
+                messages.error(request, e)
+                print(e)
+                return redirect('variationDetails', pk=pk)
         except Exception as e:
-            print(e)  
+            print(e)
     return redirect('variationDetails', pk=pk)
